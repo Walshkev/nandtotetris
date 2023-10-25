be99819281444c8811412dafe0491cda89f1d03c
Kevin <walshkev@oregonstate.edu>
2023-10-25 12:20:49 -0700

    working on c04

---
+++ cplorations/C04/parser.c
@@ -0,0 +1,71 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ *
+ * [NAME] $Kevin WalshE$
+ * [TERM] FALL $YEAR$
+ *
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s)
+{
+	char s_new[sizeof(s) + 2*sizeof(char)];
+	//char s_new[MAX_LINE_LENGTH];
+
+	int i = 0;
+	for (char *s2 = s; *s2; s2++)
+	{
+		if (*s2 == '/')
+		{
+			if (*(s2 + 1) == '/')
+			{
+				continue;
+			}
+		}
+		else if (*s2 != isspace(*s2))
+		{
+			s_new[i++] = *s2;
+		}
+		s_new[i] = '\0' ;
+		
+		strcpy(s, s_new);
+
+		// your code here
+
+		
+	}
+	return s;
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments.
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE *file)
+{
+
+	// your code here
+	char line[MAX_LINE_LENGTH] = {0};
+	// unsigned int line_num = 0;
+
+	while (fgets(line, sizeof(line), file))
+	{
+		strip(line);
+		if (!*line)
+		{
+			continue;
+		}
+		printf("%s ", line);
+}

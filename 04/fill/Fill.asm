443fd8d6254ca18abcf8ab25b7ff342974c232a2
Kevin <walshkev@oregonstate.edu>
2023-10-31 12:54:39 -0700

    mult done a little on fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,12 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(Restart)
+@SCREEN
+D=A
+@0
+M=D
+
+(keyPressed)
+@KDB
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//sets the meomory value of position 2 to 0 
+@2
+M=0
+
+//sets the memory value d to the value at M[0]
+@0
+D=M
+// sets a to @END's position 
+@END
+// if d is 0 then jump to the end
+D;JEQ
+
+@1
+D=M
+@END
+// if d is 0 then jump to the end
+D;JEQ
+
+@0
+D=M
+
+
+(LOOP)
+@1
+D=M
+
+@2
+M=D+M
+
+@0
+M=M-1
+
+D=M 
+@LOOP
+// if d is greater than 1 then jump to the postion on loop 
+D;JGT
+
+(END)
+@END
+0;JMP
+
+
